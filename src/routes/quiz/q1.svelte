5b4c0daaa56ac421362a461eceaed8e58079a1dc
Clayton Kenney <clayton.kenney@gmail.com>
2020-06-30 15:18:01 -0600

    copy change

--- src/routes/quiz/q1.svelte
+++ src/routes/quiz/q1.svelte
@@ -1,11 +1,21 @@
 <script>
 import Progress from './Progress.svelte'
 	let q1Steps = [
-	{ stepNum: '1', h2: 'Webcam Challenge', name: 'Introduction', class: 'complete',
-copy: 'If your computer has a built-in camera, also known as a webcam, you should know that it can be used to spy on you. And no, this is not just science fiction as a recent survey conducted by HP shows that 10% in the U.S. either know someone whose webcam was hacked or have had their own webcam hacked.'},
-	{ stepNum: '2', h2: 'Cover Your Webcam', name: 'Permissions', class: 'incomplete',
-copy: 'Please have your webcam covered and proceed to next step. We will access to your webcam to make sure you covered the webcam properly. Click “Test” button to proceed challenge.'},
-	{ stepNum: '3', h2: 'Great! It’s properly covered.', name: 'Cover your webcam', class: 'incomplete'}
+	{ stepNum: '1', 
+	h2: 'Webcam Challenge', 
+	name: 'Introduction', 
+	class: 'complete',
+	copy: 'If your computer has a built-in camera, also known as a webcam, you should know that it can be used to spy on you. And no, this is not just science fiction as a recent survey conducted by HP shows that 10% in the U.S. either know someone whose webcam was hacked or have had their own webcam hacked.'},
+	{ stepNum: '2', 
+	h2: 'Cover Your Webcam', 
+	name: 'Permissions', 
+	class: 'incomplete',
+	copy: 'Please have your webcam covered and proceed to next step. We will access to your webcam to make sure you covered the webcam properly. Click “Test” button to proceed challenge.'},
+	{ stepNum: '3', 
+	h2: 'Great! It’s properly covered.', 
+	name: 'Cover your webcam', 
+	class: 'incomplete',
+	copy: 'Please remember to leave your webcam covered while its not being used. This can prevent...'}
 	]
 	let q = 0;
 	function handleMessage(event) {
